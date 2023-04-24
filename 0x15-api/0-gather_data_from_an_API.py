0487ccecad248c593a85b3b00a188c793e15b446
JeremyWarui <jerywarne7@gmail.com>
2023-04-24 08:38:14 -0700

    fetch user and tasks done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #script that returns employee info on tasks progress
 from sys import argv
-from get import requests
+import requests
 
 
 if __name__ == "__main__":
@@ -9,16 +9,20 @@ if __name__ == "__main__":
 	todos_url = "https://jsonplaceholder.typicode.com/todos"
 	
 	user = "{}/{}".format(users_url, argv[1])
-	response = get(user)
+	response = requests.get(user)
 	res_json = response.json()
 	print("Employee {} is done with tasks".format(res_json.get('name')), end="")
 
 	todos = "{}?userId={}".format(todos_url, argv[1])
-	res = get(todos)
+	res = requests.get(todos)
 	tasks = res.json()
 	done_tasks = []
 	for task in tasks:
 		if task.get('completed') is True:
 			done_tasks.append(task)
+	no_of_tasks = len(tasks)
+	no_of_done_tasks = len(done_tasks)
 
-	
+	print("({}/{}:)".format(no_of_done_tasks, no_of_tasks))
+	for task in done_tasks:
+		print("\t {}".format(task.get('title')))
